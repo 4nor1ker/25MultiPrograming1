0864dff6a4565ccc5136142742e6994136b7a2fa
jy1212 <4nor1ker@gmail.com>
2025-04-15 14:14:36 +0900

    test1

---
+++ MidtermTest/20251933/test.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <windows.h>
+
+void gotoxy(int x, int y) {
+	COORD pos = { x, y };
+	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+}
+
+int main() {
+
+    gotoxy(35, 1); printf("(\033[47m    \033[0m)");
+    printf("");
+    printf("");
+    printf("");
+    printf("");
+    printf("");
+    printf("");
+    printf("");
+    printf("");
+    printf("");
+
+	return 0;
+}
